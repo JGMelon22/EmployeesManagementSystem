fda269c4a69837b1017f674f0ce08aee067568d5
JGMelon22 <jgvasconcellos22@gmail.com>
2023-05-11 13:39:35 -0300

    Enabling CORS

--- EmployeesApi/Program.cs
+++ EmployeesApi/Program.cs
@@ -2,12 +2,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Adding CORS service
+builder.Services.AddCors();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Adding CORS 
+app.UseCors(c =>
+{
+    c.WithOrigins("http://localhost:3000");
+    c.AllowAnyHeader();
+    c.AllowAnyMethod();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
