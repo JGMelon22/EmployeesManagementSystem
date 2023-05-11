13d34e8483821d1c2832b8eca05b521b7dde4ed6
JGMelon22 <jgvasconcellos22@gmail.com>
2023-05-11 18:38:10 -0300

    Get Request with axios and UseEffect to display data from API to Employee table

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # EmployeesManagementSystem 
 A simple system to register employees made with ASP .NET Core 6 and React
 
+![image](https://github.com/JGMelon22/EmployeesManagementSystem/assets/73988556/e42562cb-dfb2-4151-8780-3d08e0b6f171)
+
 <h3>NuGet Packages</h3>
 - FluentValidation.AspNetCore
 - dapper
--- employees-view/src/App.js
+++ employees-view/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import addEmployeeLogo from './assets/create-logo.png';
+import axios from 'axios';
 
 function App() {
+
+  // Api base Url
+  const baseUrl = "https://localhost:7076/api/employees";
+
+  const [data, setData] = useState([]);
+
+  // Get Request
+  const getEmployees = async () => {
+    try {
+      const response = await axios.get(baseUrl);
+      setData(response.data.data); // Get Employees data 
+    } catch (error) {
+      console.log(error);
+      setData([]);
+    }
+  }
+
+  // Use Effect to work with server effects, 
+  useEffect(() => {
+    getEmployees()
+  }, []);
+
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <a className="navbar-brand m-1" href='http://localhost:3000'>Employees System</a>
-        <ul class="navbar-nav me-auto" />
+        <ul className="navbar-nav me-auto" />
       </nav>
       <header id="CreateHeader">
         <img id="AddEmployeeLogo" src={addEmployeeLogo} alt='Create' />
@@ -21,24 +44,25 @@ function App() {
             <th>Name</th>
             <th>Age</th>
             <th>Is Active?</th>
-            <th>Business Area</th>
+            {/* <th>Business Area</th> */}
             <th className='text-center'>Options</th>
           </tr>
         </thead>
         <tbody>
           {/* Map Get Request Data from API */}
-          <tr>
-            <td>1</td>
-            <td>John Doe</td>
-            <td>20</td>
-            <td>☑</td>
-            <td>IT</td>
-            <td className='text-center'>
-              <button className='btn btn-primary'/*{onClick={}}*/>Edit</button> {" "}
-              <button className='btn btn-danger' /*{onClick={}}*/>Delete</button>
-            </td>
-          </tr>
-
+          {data.map((employee, index) => (
+            <tr key={index}>
+              <td>{employee.id}</td>
+              <td>{employee.name}</td>
+              <td>{employee.age}</td>
+              <td>{employee.active ? 'Yes' : 'No'}</td>
+              {/* <td>IT</td> */}
+              <td className='text-center'>
+                <button className='btn btn-info text-white'/*{onClick={}}*/>Edit</button> {" "}
+                <button className='btn btn-danger' /*{onClick={}}*/>Delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
