e3c7ed23bd5f50923e3543dbcad481e4c3b95fa1
JGMelon22 <jgvasconcellos22@gmail.com>
2023-05-12 15:26:36 -0300

    Developing post request using axios

--- employees-view/src/App.js
+++ employees-view/src/App.js
@@ -15,20 +15,50 @@ function App() {
   // include modal states
   const [includeModal, setIncludeModal] = useState(false);
 
+  // Obtains data from inputs
+  const [selectedEmployee, setSelectedEmployee] = useState({
+    id: '',
+    name: '',
+    age: '',
+    active: 0
+  });
+
   // Open and close include modal 
   const openCloseIncludeModal = () => {
     setIncludeModal(!includeModal);
   }
 
+  // Get input from include modal
+  const handleChange = e => {
+    const { name, value, type, checked } = e.target
+    setSelectedEmployee({
+      ...selectedEmployee, // Evaluate checkbox to know if it is a active or not employee
+      [name]: type === 'checkbox' ? (checked ? 1 : 0) : value
+    })
+  }
+
   // Get Request
   const getEmployees = async () => {
-    try {
-      const response = await axios.get(baseUrl);
-      setData(response.data.data); // Get Employees data 
-    } catch (error) {
-      console.log(error);
-      setData([]);
-    }
+    await axios.get(baseUrl)
+      .then(response => {
+        setData(response.data.data)
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
+  // Post Request
+  const postEmployees = async () => {
+    delete selectedEmployee.id;
+    selectedEmployee.id = parseInt(selectedEmployee.age);
+    selectedEmployee.active = selectedEmployee.active ? true : false; // 1 : 0
+    await axios.post(baseUrl, selectedEmployee)
+      .then(response => {
+        setData(data.concat(response.data));
+        openCloseIncludeModal();
+      }).catch(error => {
+        console.log(error.response.data);
+      });
   }
 
   // Use Effect to work with server effects, 
@@ -53,7 +83,6 @@ function App() {
             <th>Name</th>
             <th>Age</th>
             <th>Is Active?</th>
-            {/* <th>Business Area</th> */}
             <th className='text-center'>Options</th>
           </tr>
         </thead>
@@ -65,7 +94,6 @@ function App() {
               <td>{employee.name}</td>
               <td>{employee.age}</td>
               <td>{employee.active ? 'Yes' : 'No'}</td>
-              {/* <td>IT</td> */}
               <td className='text-center'>
                 <button className='btn btn-info text-white'/*{onClick={}}*/>Edit</button> {" "}
                 <button className='btn btn-danger' /*{onClick={}}*/>Delete</button>
@@ -81,18 +109,18 @@ function App() {
         <ModalBody className='form-group'>
           <label>Name</label>
           <br />
-          <input type='text' className='form-control' name='name'></input>
+          <input type='text' className='form-control' name='name' onChange={handleChange}></input>
           <br />
           <label>Age</label>
           <br />
-          <input type='number' className='form-control' name='age'></input>
+          <input type='number' className='form-control' name='age' onChange={handleChange}></input>
           <br />
           <label>Is Active?</label>
           <br />
-          <input type='checkbox' className='form-check-input' name='active'></input>
+          <input type='checkbox' className='form-check-input' name='active' onChange={handleChange}></input>
         </ModalBody>
         <ModalFooter>
-          <button className='btn btn-success'>Register</button>
+          <button className='btn btn-success' onClick={() => postEmployees()}>Register</button> {" "}
           <button className='btn btn-danger' onClick={() => openCloseIncludeModal()}>Cancel</button>
         </ModalFooter>
       </Modal>
